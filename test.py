c3960e2a48b94ae7fb78a056456fa911f86a08ba
Stanislav Osichniuk <stanhjrpower@gmail.com>
2021-10-12 17:20:57 +0300

    add 10 tests

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 
 import unittest
 from unittest.mock import patch
+from exceptions import GameOver
 from unittest import mock
 import exceptions
+from game import name_input
 from models import Player
 from models import Enemy
+
+
+class TestGame(unittest.TestCase):
+    def test_name_input(self):
+        with patch('builtins.input', return_value='stan'):
+            self.assertEqual(name_input(), 'Stan')
 
 
 class TestEnemy(unittest.TestCase):
@@ -26,20 +34,36 @@ class TestEnemy(unittest.TestCase):
 
 class TestPlayer(unittest.TestCase):
     def setUp(self) -> None:
-        self.new_player = Player('stan')
+        self.player = Player('stan')
         self.enemy_obj = Enemy(5)
 
     def test_fight(self):
-        self.assertEqual(self.new_player.fight(1, 3), -1)
+        self.assertEqual(self.player.fight(1, 3), -1)
 
     def test_input_validation(self):
         for var in range(1, 4):
             user_input = str(var)
             with patch('builtins.input', return_value=user_input):
-                self.assertEqual(self.new_player.input_validation('hi'), var)
+                self.assertEqual(self.player.input_validation('hi'), var)
+
+    def test_decrease_lives(self):
+        self.player.decrease_lives()
+        self.assertEqual(self.player.lives, 4)
+
+    def test_decrease_lives_error(self):
+        self.player.lives = 1
+        self.assertRaises(GameOver, self.player.decrease_lives)
+
+    def test_attack(self):
+        ...
+
+    def test_defence(self):
+        ...
 
 
+class TestException(unittest.TestCase):
+    ...
 
 
-
-
+if __name__ == '__main__':
+    unittest.main()
